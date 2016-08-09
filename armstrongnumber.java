84520d9624d2afb873e92e465a420acfbc53e6af
Jomari Amago <jomarib.amago@gmail.com>
2016-08-09 21:35:38 +0800

    Update armstrongnumber.java

--- armstrongnumber.java
+++ armstrongnumber.java
@@ -2,16 +2,19 @@ import java.util.*;
 class main{
 	public static void main(String[]args){
 		Scanner s = new Scanner(System.in);
-		double a=0;
-		System.out.print("Enter a three digit number:");
-		int i = s.nextInt();
-		int y = i; //passed the value of i to y
-		while(y>0){
-			int n = (y%10); //will get the modulo of the input number
-			y = y/10; //get the quotient of the input number
-			double x = Math.pow(n,3); //raise to three the inputted number
-			a = a+x; //add the cubes of the number, repeat until the the y is less than 0
-		}
-			System.out.println((i==a)? i+" is an Armstrong number":i+" is not an Armstrong number");//compared if the sum of all cubes and the inputted number is equal
+		double sum = 0;
+		int cases = s.nextInt();
+			for(int i=1;i<=cases;i++){
+				sum = 0;
+				int num = s.nextInt();
+				int pass = num;
+				while(pass>0){
+				int mod = pass%10;
+				pass = pass/10;
+				double raise = Math.pow(mod,3);
+				sum = sum + raise;
+				}
+				System.out.println((num==sum)? num+ " is an Armstrong number": num+" is not an Armstrong number");
+			}	
 	}
 }
