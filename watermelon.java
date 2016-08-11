f1bd738c325e5f48faf0ef1780bfb1c14487079a
Jomari Amago <jomarib.amago@gmail.com>
2016-08-11 23:10:42 +0800

    Delete watermelon.java

--- watermelon.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-class main{
-	public static void main(String[]args){
-	Scanner s = new Scanner(System.in);
-	System.out.print("Input: ");
-	int n = s.nextInt();
-	System.out.print((n%2==0 && n>0 && n<=100)? "Yes":"No");//A ternary condition whether a number is even or odd
-	}
-}
