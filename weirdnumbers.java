03ca6a4df79020c33856f584742aeca49683e758
Jomari Amago <jomarib.amago@gmail.com>
2016-08-11 23:10:51 +0800

    Delete weirdnumbers.java

--- weirdnumbers.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-class main{
-	public static void main(String[]args){
-		Scanner s = new Scanner(System.in);
-		System.out.print("Enter a number: ");
-		int n = s.nextInt();
-		System.out.print((n>20 || n ==2 || n==4)? "Not weird": "Weird"); //A ternary conditon that determines whether a number is weird or not
-	}
-}
